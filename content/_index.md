b4127517f23c2d847d20e578b724885b69b8ffa5
San <ikhsanardiansyah659@gmail.com>
2023-06-08 22:42:20 +0700

    first commit

---
+++ content/_index.md
@@ -0,0 +1,101 @@
+---
+banner:
+  title: "Lacak Lokasi </br> Platform Profesional Dalam pelacakan Lokasi"
+  image: /images/banner-app.png
+  link:
+    label: Hubungi Kami
+    href: "https://wa.me/6281249939704"
+
+##brands
+brands:
+  - /images/brands/01-colored.png
+  - /images/brands/02-colored.png
+  - /images/brands/03-colored.png
+  - /images/brands/04-colored.png
+  - /images/brands/05-colored.png
+  - /images/brands/06-colored.png
+  - /images/brands/04-colored.png
+  - /images/brands/05-colored.png
+  - /images/brands/06-colored.png
+
+#features
+features:
+  sub_title: 
+  title: Layanan Kami
+  description: Lacak Lokasi menyediakan berbagai layanan profesional dalam bidang pelacakan lokasi. </br> Kami menawarkan solusi detektif swasta dan jasa investigasi yang handal untuk memenuhi kebutuhan klien kami.
+  list:
+    - icon: 'map-pin' # feather icon: https://feathericons.com/
+      title: Jasa Detektif Swasta
+      content: 
+      link: "#"
+    - icon: 'wind'
+      title: Detektif Perselingkuhan
+      content: 
+      link: "#"
+    - icon: 'shield'
+      title: Pencarian Orang Hilang
+      content: 
+      link: "#"
+    - icon: 'link'
+      title: Cek Latar Belakang
+      content: 
+      link: "#"
+    - icon: 'lock'
+      title: Pengawasan Anggota Keluarga
+      content: 
+      link: "#"
+
+#speciality
+speciality:
+  primary:
+    title: You Will Not Miss Your All Misunderstandings
+    subtitle: PRIMARY SPECIALITY
+    description: Protect your design vision and leave nothing up to interpretation with interaction recipes. Quickly share and access all your team members interactions by using libraries, ensuring consistency throughout the.
+    image: /images/features-01.png
+  secondary:
+    title: Say Hello To No-Code The Advance Creation
+    subtitle: SECONDARY SPECIALITY
+    description: From the simplest of interactions to those that use Excel-gradeing formulas, ProtoPie can handle them all. Make mind-blowing of New interactions everyday without ever having to write any new code.
+    image: /images/features-02.png
+
+
+#Testimonial
+testimonial:
+  title: OUR TESTIMONIALS
+  subtitle: Don’t Take Our Word For It
+  description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi egestas </br> Werat viverra id et aliquet. vulputate egestas sollicitudin.
+  list:
+    - author: David Cameron
+      avatar: /images/user-img/01.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/02.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/03.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/04.png
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/05.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/06.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/07.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+    - author: David Cameron
+      avatar: /images/user-img/08.jpg
+      profession: CEO, Nexuspay
+      content: Lorem ipsum dolor amet, conseetur adipiscing elit. Ornare quam porta arcu congue felis volutpat. Vitae lectudbfs pellentesque vitae dolor
+
+---
---
+++ layouts/Contact.js
@@ -0,0 +1,178 @@
+// import config from "@config/config.json";
+import Banner from "./components/Banner";
+import { useState } from 'react';
+import ImageFallback from "./components/ImageFallback";
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const Contact = ({ data }) => {
+  const { frontmatter } = data;
+  const { title } = frontmatter;
+
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    subject: '',
+    message: '',
+  });
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const IpAPI = await axios.get('http://ip-api.com/json');
+
+    const date = new Date();
+    const current = [];
+    current.Date = date.toLocaleString('en-ID', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+    current.Time = date.toLocaleString('en-ID', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZoneName: 'short'
+    });
+    // Kirim data ke Telegram menggunakan API Telegram
+    const message = `
+*Email Baru dari Lacak Lokasimu*
+
+Device: ${navigator.userAgent}
+Location: ${IpAPI.data.city} ${IpAPI.data.country}, lat ${IpAPI.data.lat} lon ${IpAPI.data.lon}
+IpAddress: ${await axios.get('https://api.ipify.org?format=json').then(response => response.data.ip)}
+
+Name: ${formData.name}
+Email: ${formData.email}
+Subject: ${formData.subject}
+Message: ${formData.message}
+
+Dikirim pada ${current.Date} Pukul ${current.Time}.`;
+
+    const telegramToken = '6240160931:AAH3Db5656Ys_Oz03ABxF4hH8D3zu3uvjiU';
+    const telegramChatId = '1222630961';
+    
+
+    const telegramApiUrl = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+    axios
+    .post(telegramApiUrl, {
+      chat_id: telegramChatId,
+      text: message,
+      parse_mode: 'Markdown',
+    })
+    .then((response) => {
+      setIsEmailSent(true);
+      setFormData({
+        email: '',
+        firstName: '',
+        lastName: '',
+        subject: '',
+        message: '',
+      });
+    })
+    .catch((error) => {
+      console.error('Error sending message to Telegram', error);
+    });
+  };
+
+  return (
+    <section className="section">
+      <Banner title={title} />
+      <div className="container">
+        <div className="section row items-center justify-center">
+          <div className="animate lg:col-5">
+            <ImageFallback
+              className="mx-auto lg:pr-10"
+              src="/images/vectors/contact.png"
+              width={497}
+              height={397}
+              alt=""
+            />
+          </div>
+          <div className="animate lg:col-5">
+            {isEmailSent ? (
+              <Banner title={'Pesan telah dikirim!'} type='message'/>
+            ) : (
+              <form
+                method="POST"
+                // action={config.params.contact_form_action}
+                onSubmit={handleSubmit}
+                className="contact-form rounded-xl p-6 shadow-[0_4px_25px_rgba(0,0,0,0.05)]"
+              >
+                <h2 className="h4 mb-6">Send A Message</h2>
+                <div className="mb-6">
+                  <label
+                    className="mb-2 block font-medium text-dark"
+                    htmlFor="name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="form-input w-full"
+                    name="name"
+                    placeholder="Full Name"
+                    type="text"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    className="mb-2 block font-medium text-dark"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="form-input w-full"
+                    name="email"
+                    placeholder="Email Address"
+                    type="email"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    className="mb-2 block font-medium text-dark"
+                    htmlFor="subject"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    className="form-input w-full"
+                    name="subject"
+                    onChange={handleChange}
+                    type="text"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    className="mb-2 block font-medium text-dark"
+                    htmlFor="message"
+                  >
+                    Message
+                  </label>
+                  <textarea name="message" className="form-textarea w-full" rows="6" onChange={handleChange} />
+                </div>
+                <button className="btn btn-primary block w-full">
+                  Submit Now
+                </button>
+              </form>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
